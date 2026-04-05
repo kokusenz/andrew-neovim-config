d5f616e8514e2302dd8058666d460bd8c0f277ce
agil <andrewsebing@gmail.com>
2026-04-05 13:27:58 -0400

    remove lua_ls monkey patch; idrk what it does, makes config hard to read

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -83,26 +83,6 @@ vim.lsp.config('lua_ls', {
 
 vim.lsp.enable({'lua_ls'})
 
--- monkey-patch to fix lua_ls double go_definition issue https://github.com/LuaLS/lua-language-server/issues/2451
-local locations_to_items = vim.lsp.util.locations_to_items
-vim.lsp.util.locations_to_items = function (locations, offset_encoding)
-  local lines = {}
-  local loc_i = 1
-  for _, loc in ipairs(vim.deepcopy(locations)) do
-    local uri = loc.uri or loc.targetUri
-    local range = loc.range or loc.targetSelectionRange
-    if lines[uri .. range.start.line] then -- already have a location on this line
-      table.remove(locations, loc_i) -- remove from the original list
-    else
-      loc_i = loc_i + 1
-    end
-    lines[uri .. range.start.line] = true
-  end
-
-  return locations_to_items(locations, offset_encoding)
-end
--- end monkey patch
-
 --vim.lsp.enable({'angularls'})
 
 vim.lsp.enable('clangd')
