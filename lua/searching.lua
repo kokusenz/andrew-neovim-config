6e74c28b015b66dc77f6766f14a52fbc47f709ff
agil <andrewsebing@gmail.com>
2026-04-22 11:36:19 -0400

    update vanilla searching (fd, grep) config

--- lua/searching.lua
+++ lua/searching.lua
@@ -1,32 +1,35 @@
--- file search --
-local all_files
-
-if vim.fn.executable('fd') == 1 then--- Custom find function using fd for Vim's :find command
-  all_files = vim.fn.systemlist('fd --type f --hidden --exclude .git')
-elseif vim.fn.executable('fdfind') == 1 then
-  all_files = vim.fn.systemlist('fdfind --type f --hidden --exclude .git')
-else
-  -- fallback to default behavior
-  all_files = nil
-end
---- Searches for files using fd and filters results by pattern match
---- @param cmdarg string The search pattern to filter files (case-insensitive)
---- @param cmdcomplete any Command completion argument (unused)
---- @return table Array of matching file paths
-function UseFd(cmdarg, cmdcomplete)
-    -- Filter files that match the cmdarg pattern
-    local matches = vim.tbl_filter(function(file)
-        return string.find(file:lower(), cmdarg:lower(), 1, true) ~= nil
-    end, all_files)
-
-    return matches
-end
-
-if all_files then
-    vim.o.findfunc = 'v:lua.UseFd'
-end
-
 -- grep --
 vim.opt.grepprg = 'rg --vimgrep --no-messages --smart-case --hidden'
 vim.cmd([[cabbrev gr silent! grep!]])
 vim.cmd([[cabbrev co \| copen]])
+-- intentionally overwritten by fzf-lua module, if loaded.
+vim.keymap.set('n', '<leader>gr', function()
+    vim.ui.input({prompt = "Grep For: "}, function(input)
+        vim.cmd("silent! grep! " .. input .. " | copen")
+    end)
+end)
+
+-- file search --
+local fdfunc
+
+if vim.fn.executable('fd') == 1 then--- Custom find function using fd for Vim's :find command
+  fdfunc = 'fd'
+elseif vim.fn.executable('fdfind') == 1 then
+  fdfunc = 'fdfind'
+else
+    vim.notify('fd not found on system. :find will not use fd', vim.log.levels.WARN)
+    vim.opt.path:append { '**' }
+    return
+end
+
+function UseFd(cmdarg, _)
+    -- Filter files that match the cmdarg pattern
+    local fdout = vim.system({ fdfunc, '--type', 'f', '--hidden', '--exclude', '.git', '--full-path', cmdarg })
+        :wait()
+    local matches = vim.split(fdout.stdout, "\n", { trimempty = true })
+    return matches
+end
+
+-- intentionally overwritten by fzf-lua module, if loaded.
+vim.keymap.set('n', '<leader><leader>', ':find ')
+vim.o.findfunc = 'v:lua.UseFd'
