1b18bf0becd322c5faffc231ca0c60419993b555
agil <andrewsebing@gmail.com>
2026-04-24 14:37:14 -0400

    change dd from diag, which has default keybind, to deltaview all

--- lua/diagnostics.lua
+++ lua/diagnostics.lua
@@ -3,5 +3,3 @@ vim.diagnostic.config({ virtual_text = { current_line = true }, virtual_lines = 
 vim.keymap.set('n', '<leader>do', function()
   vim.diagnostic.enable(not vim.diagnostic.is_enabled())
 end, { silent = true, noremap = true })
-
-vim.keymap.set('n', '<leader>dd', function() vim.diagnostic.open_float() end, { silent = true, noremap = true })
--- plugin/deltaview.lua
+++ plugin/deltaview.lua
@@ -1,3 +1,4 @@
 vim.cmd([[cabbrev dm DeltaMenu]])
 vim.cmd([[cabbrev dv DeltaView]])
 vim.cmd([[cabbrev d Delta]])
+vim.keymap.set('n', '<leader>dd', function() vim.cmd([[Delta .]]) end, { silent = true, noremap = true })
