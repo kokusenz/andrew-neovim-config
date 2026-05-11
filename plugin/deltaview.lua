660f73de764376e2f758cbb078d5373bbf9bdc6a
agil <andrewsebing@gmail.com>
2026-05-11 08:42:01 -0400

    update deltaview quickfix point ref

--- plugin/deltaview.lua
+++ plugin/deltaview.lua
@@ -1,4 +1,3 @@
 vim.cmd([[cabbrev dm DeltaMenu]])
 vim.cmd([[cabbrev dv DeltaView]])
-vim.cmd([[cabbrev d Delta]])
-vim.keymap.set('n', '<leader>dd', function() vim.cmd([[Delta .]]) end, { silent = true, noremap = true })
+vim.cmd([[cabbrev de Delta .]])
