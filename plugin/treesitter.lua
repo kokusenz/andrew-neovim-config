f5baedfb1f1698f3f0cb117f6d3a9b81cbb1f547
agil <andrewsebing@gmail.com>
2026-06-03 20:55:44 -0400

    add zig treesitter

--- plugin/treesitter.lua
+++ plugin/treesitter.lua
@@ -1,8 +1,8 @@
-require'nvim-treesitter'.install({ 'lua', 'vim', 'c_sharp', 'javascript', 'typescript', 'python', 'html', 'css', 'scss', 'yaml', 'json', 'markdown', 'rust', 'cpp', 'bash' })
+require'nvim-treesitter'.install({ 'lua', 'vim', 'c_sharp', 'javascript', 'typescript', 'python', 'html', 'css', 'scss', 'yaml', 'json', 'markdown', 'rust', 'cpp', 'bash', 'zig' })
 
 -- Enable treesitter highlighting for specific filetypes
 vim.api.nvim_create_autocmd('FileType', {
-    pattern = { 'lua', 'vim', 'cs', 'javascript', 'typescript', 'python', 'html', 'css', 'scss', 'yaml', 'json', 'markdown', 'rust', 'cpp', 'sh' },
+    pattern = { 'lua', 'vim', 'cs', 'javascript', 'typescript', 'python', 'html', 'css', 'scss', 'yaml', 'json', 'markdown', 'rust', 'cpp', 'sh', 'zig' },
     callback = function()
         local buf = vim.api.nvim_get_current_buf()
         vim.schedule(function()
