ca9b469ed9778a485ec47cc889f2ec3b87fb3d6a
agil <andrewsebing@gmail.com>
2026-07-15 23:15:12 -0400

    update jj glance to just look at log cuz i don't use the other ones

--- lua/jj.lua
+++ lua/jj.lua
@@ -1,44 +1,3 @@
-local quick_command_list = {
-    --- @class QCItem
-    log = {
-        formatted = 'log',
-        on_select = function() require('floating').execute_terminal_floating('jj log') end
-    },
-    --- @type QCItem
-    st = {
-        formatted = 'status',
-        on_select = function() require('floating').execute_terminal_floating('jj st') end
-    },
-    --- @type QCItem
-    diff = {
-        formatted = 'diff',
-        on_select = function() require('floating').execute_terminal_floating('jj diff', 'center') end
-    },
-    describe = {
-        formatted = 'describe',
-        on_select = function() require('floating').execute_terminal_floating('jj describe') end
-    },
-}
-
-local jj_quick_command = function()
-    local items = {}
-    for k in pairs(quick_command_list) do
-        table.insert(items, k)
-    end
-    table.sort(items)
-    vim.ui.select(items, {
-        prompt = 'jj quick command > ',
-        format_item = function(item)
-            return quick_command_list[item].formatted
-        end,
-    }, function(item)
-        if not item then
-            return
-        end
-        quick_command_list[item].on_select()
-    end)
-end
-
 local jj_diff_select = function()
     local result = vim.system({'jj', 'diff', '--name-only'}):wait()
     if not result.stdout then
@@ -62,5 +21,5 @@ end
 vim.keymap.set('n', '<leader>w', function()
     require('floating').execute_terminal_floating('jj diff -- ' .. vim.fn.expand('%:p'), 'center')
 end)
-vim.keymap.set('n', 'gj', function() jj_quick_command() end)
+vim.keymap.set('n', 'gj', function() require('floating').execute_terminal_floating('jj log') end)
 vim.keymap.set('n', '<leader>j', function() jj_diff_select() end)
