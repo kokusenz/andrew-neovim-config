3477e44954e32fa7fa156b417ba500085d059121
agil <andrewsebing@gmail.com>
2026-07-31 10:27:51 -0400

    move plugin/ files to lua/

--- minimal_init.lua
+++ minimal_init.lua
@@ -1,9 +1,5 @@
 vim.cmd.packadd('nvim.difftool')
 vim.cmd.packadd('cfilter')
-local config = vim.fn.stdpath('config')
-vim.opt.rtp:remove(config)
-vim.opt.rtp:remove(config .. '/after')
-package.path = config .. '/lua/?.lua;' .. config .. '/lua/?/init.lua;' .. package.path
 require('preferences')
 require('conveniences')
 require('statusline')
